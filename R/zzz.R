3aceac0a8a3facaeb860becba72eeb3f8ae57217
Arne Henningsen <arne.henningsen@gmail.com>
2023-01-06 12:40:02 +0000

    version 1.0-2

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,10 @@
 .onAttach <- function( lib, pkg ) {
    packageStartupMessage(
-      paste0( "\nIf you have questions, suggestions, or comments ",
-         "regarding one of the 'micEcon' packages, ",
-         "please use a forum or 'tracker' at micEcon's R-Forge site:\n",
-         "https://r-forge.r-project.org/projects/micecon/"),
+      paste0( "\n", 
+        paste( format( citation( "micEconCES" ), bibtex = FALSE ), collapse = "\n" ),
+         "\nIf you have questions, suggestions, or comments ",
+         "regarding the 'micEconCES' package, ",
+         "please use the 'issue' tracker at the GitHub page of the package:\n",
+         "https://github.com/micEcon/micEconCES" ),
       domain = NULL,  appendLF = TRUE )
 }
